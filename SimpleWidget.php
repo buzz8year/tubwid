992efac2500b0afe4d12b735125f698d37a9277c
buzz8year <buzz8year@gmail.com>
2025-02-11 23:40:06 +0400

    Update SimpleWidget.php

--- SimpleWidget.php
+++ SimpleWidget.php
@@ -5,6 +5,8 @@ use yii\base\Widget;
 use yii\helpers\Html;
 use yii\helpers\Json;
 
+use frontend\widgets\yii2-js-widget\assets\SimpleWidgetAsset;
+
 class SimpleWidget extends Widget
 {
 	public function init()
@@ -30,6 +32,10 @@ class SimpleWidget extends Widget
 		// NOTE: Additional scripts can be regitered like below. As well as functions from your already registered js-files by SimpleWidgetAsset
 		$js = 'your_js_function(); let x = 0; console.log(x);';
 		$view->registerJs($js);
+
+		// NOTE: Similar for css-resources 
+		$css = 'body { color: red; }';
+		$view->registerCss($css);
 	}
 
 }
