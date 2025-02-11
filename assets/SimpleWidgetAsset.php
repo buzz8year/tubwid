495fdadeeb204c9042d5c002fda1f55de59f5896
buzz8year <buzz8year@gmail.com>
2025-02-11 23:27:16 +0400

    Update SimpleWidgetAsset.php

--- assets/SimpleWidgetAsset.php
+++ assets/SimpleWidgetAsset.php
@@ -4,16 +4,18 @@ use yii\web\AssetBundle;
 
 class SimpleWidgetAsset extends AssetBundle
 {
+	public $sourcePath = '@frontend/widgets/yii2_js_widget';
+	
 	// NOTE: Any prerequisite AssetBundle your widget might need
 	public $depends = [
 		// 'yii\web\JqueryAsset'
 	];
 	
-	public function init()
-	{
-		$this->sourcePath = __DIR__ . '/assets';
-		$this->js = 'your-script.js';
-
-		return parent::init();
-	}
+	public $css = [
+		// 'css/your-style.css',
+	];
+	
+	public $js = [
+		'js/your-script.js',
+	];
 }
