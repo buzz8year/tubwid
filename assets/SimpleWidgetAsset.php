9f5eacf99506d82f2c25a01c4796b98eeadf76d7
buzz8year <buzz8year@gmail.com>
2025-02-11 23:07:02 +0400

    Update SimpleWidgetAsset.php

--- assets/SimpleWidgetAsset.php
+++ assets/SimpleWidgetAsset.php
@@ -1,1 +1,18 @@
+namespace frontend\widgets\yii2-js-widget/assets;
 
+use yii\web\AssetBundle;
+
+class SimpleWidgetAsset extends AssetBundle
+{
+
+	public $depends = [
+		'yii\web\JqueryAsset'
+	];
+	
+	public function init()
+	{
+		$this->sourcePath = __DIR__ . '/assets';
+		$this->js = 'your-script.js';
+		return parent::init();
+	}
+}
