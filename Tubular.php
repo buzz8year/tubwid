04961e80a7d620445bd9d7c17bd9cb638ff9e7d8
buzz8year <buzz8year@gmail.com>
2025-02-11 23:03:29 +0400

    Update Tubular.php

--- Tubular.php
+++ Tubular.php
@@ -1,22 +1,17 @@
-namespace yii\tubular;
+namespace frontend\widgets\yii2-js-widget;
 
 use Yii;
 use yii\base\Widget;
-use yii\base\InvalidConfigException;
 use yii\helpers\Html;
 use yii\helpers\Json;
-use yii\web\JsExpression;
 
-class Tubular extends Widget
+class SimpleWidget extends Widget
 {
 	public function init()
 	{
 		parent::init();
-
-		if (empty($this->youtubecode)) {
-			throw new InvalidConfigException('Id видео отсутствует');
-		}
 	}
+
 	/**
 	 * @see \yii\base\Widget::run()
 	 */
@@ -28,8 +23,11 @@ class Tubular extends Widget
 	public function registerClientScript()
 	{
 		$view = $this->getView();
+
+		// NOTE: This registers all js/css resources defined witin Asset
 		TubularAsset::register($view);
 
+		// NOTE: Additiona
 		$js = 'let smth = 0; your_js_function(); console.log(smth);';
 		$view->registerJs($js);
 	}
